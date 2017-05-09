f3190751c58c7e9e4e88cbf875d7b870b3f16e6f
streco <sgreco41@gmail.com>
2017-05-08 22:50:25 -0400

    Creation of List View

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
-  state = { albums: [] }
+  state = { albums: [] }  //Set to empty array
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => this.setState({ albums: response.data }));
+      .then(response => this.setState({ albums: response.data }));  //Set response to albums
   }
-  render() {
+
+renderAlbums() {
+  //return created array, key is needed ot be unique
+  return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+}
+
+
+  render() {  //Rerendered when state changes
     console.log(this.state);
     return (
       <View>
-        <Text>Album List !!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
